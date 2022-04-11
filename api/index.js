1b89b042f06919337f9799638f13fc44bfdfb6a5
wangshijun <wangshijun2010@gmail.com>
2022-04-11 09:04:47 +0800

    fix: make gun server work

--- api/index.js
+++ api/index.js
@@ -1,13 +1,23 @@
+const path = require('path');
+const Gun = require('gun');
 const express = require('express');
 
+const env = require('./libs/env');
+
 const app = express();
+
+app.use(Gun.serve);
 
 const port = process.env.BLOCKLET_PORT || 3030;
 
 app.get('/', (req, res) => {
-  res.send('Hello World, Blocklet!');
+  res.send(`Relay Endpoint: ${env.appUrl}/gun`);
 });
 
-app.listen(port, () => {
-  console.log(`Blocklet app listening on port ${port}`);
+const server = app.listen(port, () => {
+  console.log(`gun relay ready on port ${port}`);
 });
+
+const gun = Gun({ web: server, file: path.join(env.dataDir, 'gun') });
+
+module.exports = { app, server, gun };
--- api/libs/env.js
+++ api/libs/env.js
@@ -2,5 +2,4 @@ const env = require('@blocklet/sdk/lib/env');
 
 module.exports = {
   ...env,
-  chainHost: process.env.CHAIN_HOST || '',
 };
